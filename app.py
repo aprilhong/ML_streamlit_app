47d993fc376b6920306d78b219c121b4ffdf3583
April Hong <78663820+aprilhong@users.noreply.github.com>
2024-09-06 14:48:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ import streamlit as st
 st.title('Machine Learning App')
 
 st.info('This is app builts a machine learning model')
+
+with st.expander('Data'):
+  st.write('**Raw Data')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
