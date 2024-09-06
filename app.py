ca6f92ac4ab2b3724c37e0425bb8c9fffd4d0d2a
April Hong <78663820+aprilhong@users.noreply.github.com>
2024-09-06 15:14:44 -0400

    complete input widgets

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ with st.expander('Data Visualization'):
 with st.sidebar:
   st.header('Input Features')
   island= st.selectbox('Island',('Biscoe','Dream','Torgersen'))
-  sex = st.selectbox('Gender',('Male','Female'))
+  sex = st.selectbox('Gender',('male','female'))
   bill_length_mm = st.slider('Bill Length (mm)', 32.1, 59.6, 43.9)
-  # bill_depth_mm
-  # flipper_length_mm
-  # body_mass_g
+  bill_depth_mm = st.slider('Bill Depth (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Flipper Length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body Mass (g)', 2700.0, 6300.0, 4207.0)
 
