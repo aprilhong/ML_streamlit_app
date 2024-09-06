3a9859c7bda9f1749b4bf568990129b8abf3be24
April Hong <78663820+aprilhong@users.noreply.github.com>
2024-09-06 15:19:11 -0400

    input df

--- app.py
+++ app.py
@@ -28,9 +28,23 @@ with st.expander('Data Visualization'):
 with st.sidebar:
   st.header('Input Features')
   island= st.selectbox('Island',('Biscoe','Dream','Torgersen'))
-  sex = st.selectbox('Gender',('male','female'))
   bill_length_mm = st.slider('Bill Length (mm)', 32.1, 59.6, 43.9)
   bill_depth_mm = st.slider('Bill Depth (mm)', 13.1, 21.5, 17.2)
   flipper_length_mm = st.slider('Flipper Length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('Body Mass (g)', 2700.0, 6300.0, 4207.0)
+  sex = st.selectbox('Gender',('male','female'))
 
+
+  # Create dataframe for input features
+  data = {'island': island,
+        'bill_length_mm': bill_length_mm,
+        'bill_depth_mm': bill_depth_mm,
+        'flipper_length_mm': flipper_length_mm,
+        'body_mass_g': body_mass_g,
+        'sex': sex
+        }
+
+  df_input = pd.DataFrame(data, index=[0])
+  input_penguins = pd.concat([df_input,X], axis=0)
+
+df_input
